664641bb255c14c1a05d57d46c347026a85f9892
jkastrem <drdlemmer@gmail.com>
2022-02-27 14:06:48 +0300

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-strongly-recommended',
+  ],
+  rules: {
+    'semi': [2, 'never'],
+  }
+}
