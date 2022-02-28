220fff91f132327eefbdf1de5c42fb995177b1a0
jkastrem <drdlemmer@gmail.com>
2022-03-01 02:49:09 +0300

    Add switcher button for lists

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <script setup>
+import { computed, reactive, ref } from 'vue'
 import { pokemonsStore as usePokemonsStore } from './store/pokemons'
 
 import TheButton from './components/common/TheButton.vue'
 import PokemonCard from './components/PokemonCard.vue'
 
 const pokemonsStore = usePokemonsStore()
+
+const availableList = ref({
+  simpleList: 'simpleList',
+  favoriteList: 'favoriteList',
+})
+
+const localState = reactive({
+  currentList: availableList.value.simpleList,
+})
+
+function setCurrentListToFavorite() {
+  localState.currentList = availableList.value.favoriteList
+}
+
+function setCurrentListToSimple() {
+  localState.currentList = availableList.value.simpleList
+}
+
+const setterCurrentListFunction = new Map()
+setterCurrentListFunction.set(availableList.value.simpleList, setCurrentListToFavorite)
+setterCurrentListFunction.set(availableList.value.favoriteList, setCurrentListToSimple)
+
+const buttonSwitcherListText = new Map()
+buttonSwitcherListText.set(availableList.value.simpleList, 'Go to favorites')
+buttonSwitcherListText.set(availableList.value.favoriteList, 'Go to simple list')
+
+const switchSetterCurrentList = computed(() => {
+  return setterCurrentListFunction.get(localState.currentList)
+})
+const switcherCurrentListText = computed(() => {
+  return buttonSwitcherListText.get(localState.currentList)
+})
+
 pokemonsStore.refreshPokemonList()
 </script>
 
 <template>
   <div class="flex justify-center">
     <TheButton
-      text="Go to Favorites"
+      :text="switcherCurrentListText"
       class="mt-4"
+      @click="switchSetterCurrentList()"
     />
   </div>
   <div class="flex flex-wrap items-center justify-center">
