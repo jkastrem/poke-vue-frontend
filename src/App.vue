a3e33de52f3741bb62369b27ea0b96f5ba0804dc
jkastrem <drdlemmer@gmail.com>
2022-03-01 02:08:17 +0300

    Add button 'Go to favorites'

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { pokemonsStore as usePokemonsStore } from './store/pokemons'
+
+import TheButton from './components/common/TheButton.vue'
 import PokemonCard from './components/PokemonCard.vue'
 
 const pokemonsStore = usePokemonsStore()
@@ -7,6 +9,12 @@ pokemonsStore.refreshPokemonList()
 </script>
 
 <template>
+  <div class="flex justify-center">
+    <TheButton
+      text="Go to Favorites"
+      class="mt-4"
+    />
+  </div>
   <div class="flex flex-wrap items-center justify-center">
     <PokemonCard 
       v-for="pokemon in pokemonsStore.pokemonsList"
