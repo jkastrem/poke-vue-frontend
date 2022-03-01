9fc818b0009f17aeda7cb34975c2100814d30369
jkastrem <drdlemmer@gmail.com>
2022-03-02 02:24:06 +0300

    Fix bugs with non-show list when page is loading

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,56 @@
 <script setup>
-import { computed, reactive, ref } from 'vue'
+import { computed, onBeforeMount, reactive } from 'vue'
 import { pokemonsStore as usePokemonsStore } from './store/pokemons'
+import { availableList, buttonSwitcherListText } from './maps'
 
 import TheButton from './components/common/TheButton.vue'
 import PokemonCard from './components/PokemonCard.vue'
 
 const pokemonsStore = usePokemonsStore()
 
-const availableList = ref({
-  simpleList: 'simpleList',
-  favoriteList: 'favoriteList',
-})
-
 const localState = reactive({
-  currentList: availableList.value.simpleList,
+  currentList: availableList.simpleList,
 })
 
 function setCurrentListToFavorite() {
-  localState.currentList = availableList.value.favoriteList
+  localState.currentList = availableList.favoriteList
 }
 
 function setCurrentListToSimple() {
-  localState.currentList = availableList.value.simpleList
+  localState.currentList = availableList.simpleList
 }
 
-const setterCurrentListFunction = new Map()
-setterCurrentListFunction.set(availableList.value.simpleList, setCurrentListToFavorite)
-setterCurrentListFunction.set(availableList.value.favoriteList, setCurrentListToSimple)
-
-const buttonSwitcherListText = new Map()
-buttonSwitcherListText.set(availableList.value.simpleList, 'Go to favorites')
-buttonSwitcherListText.set(availableList.value.favoriteList, 'Go to simple list')
-
-const switchSetterCurrentList = computed(() => {
-  return setterCurrentListFunction.get(localState.currentList)
-})
-const switcherCurrentListText = computed(() => {
-  return buttonSwitcherListText.get(localState.currentList)
+onBeforeMount(() => {
+  pokemonsStore.refreshPokemonList()
 })
 
-pokemonsStore.refreshPokemonList()
+const pokemonList = computed(() => {
+  if (localState.currentList === availableList.favoriteList) return pokemonsStore.favoritePokemons
+  else return pokemonsStore.pokemonsList
+})
+
+const switcherFunction = computed(() => {
+  return localState.currentList === availableList.simpleList
+      ? setCurrentListToFavorite
+      : setCurrentListToSimple
+})
+
+const buttonText = computed(() => {
+  return buttonSwitcherListText[localState.currentList]
+})
 </script>
 
 <template>
   <div class="flex justify-center">
     <TheButton
-      :text="switcherCurrentListText"
+      :text="buttonText"
       class="mt-4"
-      @click="switchSetterCurrentList()"
+      @click="switcherFunction"
     />
   </div>
   <div class="flex flex-wrap items-center justify-center">
     <PokemonCard 
-      v-for="pokemon in pokemonsStore.pokemonsList"
+      v-for="pokemon in pokemonList"
       :key="pokemon.id"
       v-bind="pokemon"
     />
---
+++ src/maps.js
@@ -0,0 +1,9 @@
+export const availableList = {
+  simpleList: 'simpleList',
+  favoriteList: 'favoriteList',
+}
+
+export const buttonSwitcherListText = {
+  [availableList.simpleList]: 'Go to favorites',
+  [availableList.favoriteList]: 'Go to simples',
+}
