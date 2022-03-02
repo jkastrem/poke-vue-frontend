cdecbad4c854e84fd459d633cde783d68706a8af
jkastrem <drdlemmer@gmail.com>
2022-03-02 17:19:28 +0300

    Add test; fix filter

---
+++ tests/filter.spec.js
@@ -0,0 +1,18 @@
+import { expect, test } from 'vitest'
+import { filterArrayByField } from '../src/helpers/array'
+
+test('it should be return array from 5 to 10 including', () => {
+  const willFilterArray = [
+    { id: 1, field: 15 },
+    { id: 2, field: 5 },
+    { id: 3, field: 7 },
+  ]
+
+  expect(filterArrayByField(willFilterArray, 'field', 5, 10)).toEqual(
+    [
+      { id: 2, field: 5 },
+      { id: 3, field: 7 },
+      { id: 1, field: 15 },
+    ],
+  )
+})
